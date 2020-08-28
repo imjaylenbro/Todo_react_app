21b6085c5ecf4d73a91e0895bbd70feeb113a221
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-08-27 17:58:41 -0700

    added content to js and css

--- todo_react-app/src/App.js
+++ todo_react-app/src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <body>
+    <div class="topBar">
+      
+      <h1 class="titleText">Task It</h1>
     </div>
+    
+      <h2 class="dateText">Tuseday, August 26 </h2>
+        <p class= "quoteText">
+          "Change will not come if we waint for some other person or if we wait for other time. 
+          We are the ones we've been waiting for. We are the change that we seek."
+          <br/>
+          <br/>
+          -Barack Obama
+         </p>
+      <ul>
+        <li class="taskBox"><input type="checkbox"/>Mediatate</li>
+        <br></br>
+        <li class="taskBox"><input type="checkbox"/>Grocery Shopping</li>
+        <br></br>
+        <li class="taskBox"><input type="checkbox"/>Yoga</li>
+        <br/>
+        <button class="taskButton">Create New</button>
+      </ul>
+      <nav>
+      <ul class="navContainer">
+        <li><a src="#">Home</a></li>
+        <li><a scr="#">History</a></li>
+        <li><a scr="#">Profile</a></li>
+      </ul>
+    </nav>
+      </body>
+    
   );
 }
 
