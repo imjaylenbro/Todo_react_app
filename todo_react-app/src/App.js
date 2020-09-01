1cc6892580e84dfe5eebd984b01433a55ea59348
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-09-01 02:01:02 -0700

    added some JSX

--- todo_react-app/src/App.js
+++ todo_react-app/src/App.js
@@ -1,39 +1,62 @@
 import React from 'react';
 import './App.css';
 
+const toDoList = [{
+    name: "Meditate",
+    description: "Try at least two mins",
+    completed: true,
+
+},
+{ 
+  name: "Grocery Shopping",
+  description: "Because you know your partner won't.",
+  completed: false,
+},
+{
+  name: "Yoga",
+description: "It's gonna help your back.",
+completed: true,
+}];
+
+let profile = {
+  username: "Jaylen",
+  avatarImg: "https://1001freedownloads.s3.amazonaws.com/vector/thumb/99709/THE_HOOD_II.png",
+}
+
 function App() {
   return (
   
     <body>
-    <div class="topBar">
+    <div className="topBar">
       
-      <h1 class="titleText">Task It</h1>
+      <h1 className="titleText">Task It</h1>
     </div>
-    
-      <h2 class="dateText">Tuseday, August 26 </h2>
-        <p class= "quoteText">
+    <div className =" user-container">
+    <p>{profile.username}</p>
+    <img src={profile.avatarImg} alt="hooded cartoon charterç" />
+
+    </div>
+      <h2 className="dateText">Tuseday, August 26 </h2>
+        <p className= "quoteText">
           "Change will not come if we waint for some other person or if we wait for other time. 
           We are the ones we've been waiting for. We are the change that we seek."
           <br/>
           <br/>
           -Barack Obama
          </p>
-      <ul>
-        <li class="taskBox"><input type="checkbox"/>Mediatate</li>
-        <br></br>
-        <li class="taskBox"><input type="checkbox"/>Grocery Shopping</li>
-        <br></br>
-        <li class="taskBox"><input type="checkbox"/>Yoga</li>
-        <br/>
-        <button class="taskButton">Create New</button>
-      </ul>
-      <nav>
-      <ul class="navContainer">
-        <li><a src="#">Home</a></li>
-        <li><a scr="#">History</a></li>
-        <li><a scr="#">Profile</a></li>
-      </ul>
-    </nav>
+         <ul>
+           {toDoList.map((tasks) => {
+             return (
+               <li className= "taskBox">
+                <p>{tasks.name}</p>
+                <p>{tasks.description}</p>
+                <input type="checkbox"  checked={tasks.completed} />
+               </li>
+             )
+           })}
+         </ul>
+
+     
       </body>
     
   );
