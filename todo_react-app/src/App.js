e4d9cd01dc43807349c48c86ffdb936a5569ef4b
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-09-09 03:35:05 -0700

    created a app-com component

--- todo_react-app/src/App.js
+++ todo_react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import TodoList from "./components/todo-items";
+import TodoList from "./components/Todo-items";
+import Welcome from "./components/App-com";
 import "./App.css";
 
 const list = [
@@ -19,66 +20,26 @@ const list = [
     completed: true,
   },
 ];
+/* <img style={profileImg} src={profile.avatarImg} alt="hooded cartoon" /> */
 
-let profile = {
-  username: "Jaylen",
-  avatarImg:
-    "https://1001freedownloads.s3.amazonaws.com/vector/thumb/99709/THE_HOOD_II.png",
-};
 
 function App() {
   return (
-    <body>
-      <div style={topBar}>
-        <h1 style={titleText}>Task It</h1>
-      </div>
-      <div>
-        <p style={text}> Welcome back {profile.username}!</p>
-        <img style={profileImg} src={profile.avatarImg} alt="hooded cartoon" />
-      </div>
-      <h2 style={dateText}>Tuseday, August 26 </h2>
-      <p style={text}>
-        "Change will not come if we wait for some other person or if we wait for
-        other time. We are the ones we've been waiting for. We are the change
-        that we seek."
-        <br />
-        <br />
-        -Barack Obama
-      </p>
+    <>
+   
+        <Welcome/>
+     
       <ul>
         {list.map((tasks) => {
           return <TodoList list={tasks} />;
         })}
       </ul>
-    </body>
+      </>
+    
   );
 }
 
-const topBar = {
-  backgroundColor: "#1BBCE0",
-  textAlign: "center",
-  width: "100%",
-  padding: "1%",
-};
 
-const titleText = {
-  fontSize: "40px",
-  textAlign: "center",
-};
-
-const text = {
-  padding: "4%",
-  textAlign: "center",
-  fontSize: "19px",
-};
-
-const dateText = {
-  textAlign: "center",
-};
-
-const profileImg = {
-  width: "8em",
-};
 
 // const navBar = {
 //   fontSize: "17px",
---
+++ todo_react-app/src/components/App-com.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function Welcome(here){
+    return ( 
+    <>
+        <div style={styles.topBar}>
+        <h1 style={styles.titleText}>Task It</h1>
+      </div>
+    <p style={styles.mainText}> Welcome back {profile.userName}!</p>
+        <h2 style={styles.dateText}>Tuseday, August 26 </h2>
+        <p style={styles.mainText}>
+            "Change will not come if we wait for some other person or if we wait for
+            other time. We are the ones we've been waiting for. We are the change
+            that we seek."
+            <br />
+            <br />
+            -Barack Obama
+        </p>  
+      </>
+    );
+}
+export default Welcome
+
+
+const styles = {
+    topBar: {
+        backgroundColor:"#1BBCE0",
+        textAlign:"center",
+        width:"100%",
+        padding:"1%",
+    },
+    titleText:{
+        fontSize: "40px",
+        textAlign: "center",
+    },
+    mainText:{
+        padding:"4%",
+        textAlign: "center",
+        fontSize: "19px",
+    },
+    dateText: {
+        textAlign: "center",
+    } 
+
+};
+
+let profile = {
+    userName: "Jaylen",
+    avatarImg:
+      "https://1001freedownloads.s3.amazonaws.com/vector/thumb/99709/THE_HOOD_II.png",
+  };  
